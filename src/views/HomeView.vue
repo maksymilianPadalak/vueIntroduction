4e23e9fa34b10284d8162a83e8fdcca01e1cdbba
maksymilianpadalak <maksymilian.padalak@gmail.com>
2025-05-08 19:26:58 +0200

    Create Vue app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MyFirstComponent from '../components/MyFirstComponent.vue'
+</script>
+
+<template>
+  <main>
+    <MyFirstComponent msg="Hello World" />
+  </main>
+</template>
