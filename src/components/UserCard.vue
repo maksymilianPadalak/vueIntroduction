eb21dc6a422a5741fb758f4bb351dee3a1a34ce1
maksymilianpadalak <maksymilian.padalak@gmail.com>
2025-05-11 09:21:16 +0200

    Create users list

---
+++ src/components/UserCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="root">
+    <h3>Name: {{ user.name }}</h3>
+    <h3>Username: {{ user.username }}</h3>
+    <h3>Email: {{ user.email }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { User } from '@/types'
+
+defineProps<{ user: User }>()
+</script>
+
+<style scoped>
+.root {
+  padding: 20px;
+  background-color: aquamarine;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 3px solid black;
+}
+</style>
--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,26 +1,66 @@
 <template>
-  <h1>{{ title }}</h1>
-  <h2 v-if="loading">Loading...</h2>
-  <div v-if="!loading" v-for="user in users">
-    <h3>{{ user.name }}</h3>
+  <div class="root">
+    <h1>{{ title }}</h1>
+    <input v-model="searchQueryUser" placeholder="Serach for user" class="input" />
+    <h2 v-if="loading">Loading...</h2>
+    <div class="userCardWrapper" v-if="!loading" v-for="user in displayedUsers">
+      <UserCard :user class="userCard" />
+    </div>
+    <h2 v-if="displayedUsers.length == 0">No user's name match the search query</h2>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { User } from '@/types'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
+import UserCard from './UserCard.vue'
 
 const title = 'Users List'
 const loading = ref(false)
-const users = ref<User[]>([])
+const displayedUsers = ref<User[]>([])
+let users: User[] = []
 
 onMounted(fetchData)
 
 async function fetchData() {
   loading.value = true
-  const usersData = await fetch('https://jsonplaceholder.typicode.com/users')
-  const usersParsed = await usersData.json()
-  users.value = usersParsed
-  loading.value = false
+  try {
+    const usersData = await fetch('https://jsonplaceholder.typicode.com/users')
+    const usersParsed = await usersData.json()
+    users = usersParsed
+    displayedUsers.value = users
+  } catch (error) {
+    console.error
+  } finally {
+    loading.value = false
+  }
 }
+
+const searchQueryUser = ref('')
+
+watch(searchQueryUser, () => {
+  displayedUsers.value = users.filter((user) => {
+    return user.name.toUpperCase().includes(searchQueryUser.value.toUpperCase())
+  })
+})
 </script>
+
+<style scoped>
+.root {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userCard {
+  margin: 20px;
+}
+
+.userCardWrapper {
+  width: 100%;
+}
+
+.input {
+  font-size: 20px;
+}
+</style>
